3c72bb248d2eadb292ccfb1d43f165d450307e1d
Marc Dunand <marcdunandc@gmail.com>
2024-11-10 19:42:19 -0500

    Set to default output settings

--- sketch_asemic.py
+++ sketch_asemic.py
@@ -5,30 +5,30 @@ import numpy as np
 from dataclasses import dataclass, replace
 from typing import List
 
-pageLen = 270                       #the height of our writing block in mm
-lineLen = 210                       #the maximum length of a line of text
+pageLen = 250                       #the height of our writing block in mm
+lineLen = 180                       #the maximum length of a line of text
 aphabetLen = 30                     #the number of characters in the language's alphabet
-vDiv = 6                            #number of verticle divisions there are on a character
+vDiv = 4                            #number of verticle divisions there are on a character
 dotProb = 0.3                       #chance of an accent being added in any given spot
 accLen = 0.8                        #how long horizontal strokes are
 charH = 5.5                           #maximum height of characters
-charSizeNoise = 1.2                #how much the size of letters can vary, 1 is no variation, n >= 1
-charSizeLineNoise = 1.4             #how much does the character size along an entire line vary (affects all characters on a line)
+charSizeNoise = 1.1                #how much the size of letters can vary, 1 is no variation, n >= 1
+charSizeLineNoise = 1             #how much does the character size along an entire line vary (affects all characters on a line)
 wordStdv = 10                       #standard deviation of word length
 newLineChance = 0.01                #chance that the end of a word also triggers a new line
 drawAlphabet = False                #determines if the alphabet of characters is drawn at the top
 wiggleMin, wiggleMax = -0.3, 0.4    #maximum verticle offset of one char from the next
 lineMin, lineMax = -5, 3            #maximum horizontal offset from one line to the next
-lineSpacing = 1.1                  #verticle spacing between lines. 1 is no spacing
+lineSpacing = 1.3                  #verticle spacing between lines. 1 is no spacing
 spaceLen = 1.5                        #how long spaces between words are
 cairnLen = -0.5                     #how long spaces between characters are
-charTilt = 0.2                      #how much each char's verticle lines tilt as a ratio to their height
+charTilt = 0.24                      #how much each char's verticle lines tilt as a ratio to their height
 charTiltNoise = 0.2                 #how much the tilt on a character's slashes varies
 seedLen = 50                        #how many numbers are generated to determine the shape of a char in the alphabet, just make sure this is big enough that no error occures
-maxShrink = 0.3                     #how much smaller letters get at maximum over the height of the page
+maxShrink = 0.2                     #how much smaller letters get at maximum over the height of the page
 scribbleSize = 1                    #how much larger than the characters are the scribbles that overlay them
 scribbleEnd = 0.7                   #chance of a scribble ending at the end of a word
-featureChance = 0.03                #chance of starting a feature
+featureChance = 0.0                #chance of starting a feature
 scribbleChance = 1                  #given that a feature will be drawn, what is the chance that the feature is a scribble
 
 
@@ -254,7 +254,7 @@ class AsemicSketch(vsketch.SketchClass):
 
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
-        vsk.vpype("linemerge linesimplify reloop linesort")
+        vsk.vpype("linemerge linesimplify reloop")
 
 
 if __name__ == "__main__":
